f125dad96af262142dffca82236cdc1d168745bb
gael <gael.calligaro@edu.ece.fr>
2025-04-01 16:58:14 +0200

    test gael

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         System.out.println("test victor");
 
-        System.out.println("test gael encore encore");
+        System.out.println("test gael encore encore  ");
 
 
     }
