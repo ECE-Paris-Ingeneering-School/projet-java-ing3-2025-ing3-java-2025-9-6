e155326bb5b7d5159b1e825104f7603aaf31d93c
Orphé Lafarge <131762127+OrpheLafarge@users.noreply.github.com>
2025-04-18 19:15:36 +0200

    Update Article.java

--- src/Article.java
+++ src/Article.java
@@ -1,22 +1,39 @@
+package model;
+
 public class Article {
     private int id;
     private String nom;
+    private String marque;
     private double prixUnitaire;
     private double prixVrac;
     private int quantiteVrac;
+    private int stock;
 
-    public Article(int id, String nom, double prixUnitaire, double prixVrac, int quantiteVrac) {
+    public Article(int id, String nom, String marque, double prixUnitaire, double prixVrac, int quantiteVrac, int stock) {
         this.id = id;
         this.nom = nom;
+        this.marque = marque;
         this.prixUnitaire = prixUnitaire;
         this.prixVrac = prixVrac;
         this.quantiteVrac = quantiteVrac;
+        this.stock = stock;
     }
 
-    // Getters et setters
+    // Getters
     public int getId() { return id; }
     public String getNom() { return nom; }
+    public String getMarque() { return marque; }
     public double getPrixUnitaire() { return prixUnitaire; }
     public double getPrixVrac() { return prixVrac; }
     public int getQuantiteVrac() { return quantiteVrac; }
+    public int getStock() { return stock; }
+
+    // Setters
+    public void setId(int id) { this.id = id; }
+    public void setNom(String nom) { this.nom = nom; }
+    public void setMarque(String marque) { this.marque = marque; }
+    public void setPrixUnitaire(double prixUnitaire) { this.prixUnitaire = prixUnitaire; }
+    public void setPrixVrac(double prixVrac) { this.prixVrac = prixVrac; }
+    public void setQuantiteVrac(int quantiteVrac) { this.quantiteVrac = quantiteVrac; }
+    public void setStock(int stock) { this.stock = stock; }
 }
