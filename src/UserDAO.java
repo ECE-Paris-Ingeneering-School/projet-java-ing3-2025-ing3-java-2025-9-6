dd20bbb79bc6d6c4b06ab84f84a33cbaa8187608
Bat29 <90864463+Bat2906@users.noreply.github.com>
2025-04-20 22:05:14 +0200

    Update DAO 8

---
+++ src/UserDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO implements UserDAOInterface {
+    private final Connection connection;
+}
