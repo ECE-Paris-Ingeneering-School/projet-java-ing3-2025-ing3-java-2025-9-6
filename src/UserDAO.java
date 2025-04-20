91ddd876b8cd4eebe36149a1510419dbe165e43b
Bat29 <90864463+Bat2906@users.noreply.github.com>
2025-04-20 22:05:46 +0200

    Update DAO 9

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -8,4 +8,9 @@ import java.util.List;
 
 public class UserDAO implements UserDAOInterface {
     private final Connection connection;
+
+    // Modification du constructeur pour accepter une Connection directement
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
 }
