59b250fcd6f13b6f836975a45154c8ab4d5ec7f1
Bat29 <90864463+Bat2906@users.noreply.github.com>
2025-04-20 22:00:37 +0200

    Update DAO 2

--- src/PaiementDAO.java
+++ src/PaiementDAO.java
@@ -5,4 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PaiementDAO {
+  private static final List<Paiement> paiementsEffectues = new ArrayList<>();
+
+    public static void enregistrerPaiement(Paiement paiement) {
+        paiementsEffectues.add(paiement);
+        System.out.println("Paiement enregistré : " + paiement.getNomProprietaire() + " via " + paiement.getMethode());
+    }
+
+    public static List<Paiement> getHistoriquePaiements() {
+        return paiementsEffectues;
+    }
 }
